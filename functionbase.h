a06e461cb7e776f2f008aa623df656fea16274b6
R-Return <105487743+R-Return@users.noreply.github.com>
2022-05-19 16:58:32 +0800

    Add files via upload

---
+++ functionbase.h
@@ -0,0 +1,13 @@
+#ifndef _functionbase_h
+#define _functionbase_h
+void DrawRectangle(double px,double py,double qx,double qy);
+void FillRectangle(double px,double py,double qx,double qy)
+void store();
+void RankList();
+void Exit();
+void Rank_for_exit();
+char getrandchar();
+void restart();
+void drawedge();
+#endif
+
